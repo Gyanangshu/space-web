78c085610a8081c5923f6c6c9fcc8cdba59a8e0c
Gyanangshu <babubiku@gmail.com>
2023-02-27 17:23:55 +0530

    third

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,12 +12,12 @@ const Hero = () => {
         STRATEGIES. LEARN HOW OUR STRATEGY ACHIEVED 250% BUSINESS GROWTH FOR CLIENTS.</p>
 
         </div>
-
+ 
       <div className='hero__call'>
         <button class="btn">
           <div>
-            <p className='hero__button-text p__poppins'>Get Your FREE Marketing Plan</p>
-            <p className='hero__term p__poppins'>100% Free & No Obligation</p>
+            <a href="https://calendly.com/hypermediamarketing/15min?fbclid=IwAR3HbvsMJXo1M9mFre1rlzxXayN8n-pS-REHi_-t0HaXK1ME9CLRA6VW_gU&month=2023-02"><p className='hero__button-text p__poppins'>Get Your FREE Marketing Plan</p>
+            <p className='hero__term p__poppins'>100% Free & No Obligation</p></a>
           </div> 
           <div class="icon">
             <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
